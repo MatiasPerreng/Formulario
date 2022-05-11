d8353f4e3e00bf42752b36c0afcbcc2cb2931f9a
MatiasPerreng <csmatiperreng@gmail.com>
2022-05-10 22:31:53 -0300

    ultimo comit

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php
-echo "Hola";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    <form action="datos.php" method="post" class="form">
+        
+Cedula: <input type="number" name="cedula" placeholder="Cedula" class="input">
+<br>
+Nombre: <input type="text" name="nombre" placeholder="Nombre" class="input">
+<br>
+Apellido : <input type="text" name="apellido" placeholder="Apellido" class="input">
+<br>
+Especificación: <input type="text" name="especificacion" placeholder="Especificación" class="input">
+<br>
+ <input type="submit" name="enviar" class="btn">
+    </form>
+    
+</body>
+</html>
---
+++ registrar.php
@@ -0,0 +1,17 @@
+<?php
+
+include("con_db.php");
+
+
+if(isset($_POST['enviar'])){
+if(strlen($_POST['cedula']) < 1 && strlen($_POST['nombre']) < 1 && strlen($_POST['apellido']) < 1 &&strlen($_POST['especificacion']) < 1) {
+$cedula = trim($_POST['cedula']);
+$nombre = trim($_POST['name']);
+$apellido = trim($_POST['apellido']);
+$especificacion = trim($_POST['especificacion']);
+$consulta = INSERT INTO `medico`(Cedula, Nombre, Apellido, Especificación) VALUES ('$cedula','$nombre','$apellido','$especificacion','$fechareg')
+$resultado = mysqli_query($conex,$consulta);
+
+}
+}
+?>
